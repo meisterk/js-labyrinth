4e4bf4a0ea2e6ac55c7218178d906e12d406c260
Franz Kohnle <franz@kohnlehome.de>
2021-02-28 22:42:40 +0100

    Change names

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 class Kasten {
     constructor(){
-        this.rahmenoben = false;
-        this.rahmenrechts = false;
-        this.rahmenunten = false;
-        this.rahmenlinks = false;
+        this.rahmenOben = false;
+        this.rahmenRechts = false;
+        this.rahmenUnten = false;
+        this.rahmenLinks = false;
         
         this.nachbarn = [];
         this.zustand = '⋅';        
@@ -21,31 +21,68 @@ const App = {
         }
     },
     methods:{
-        rahmensetzen(){
+        rahmenSetzen(){
             for(let spalte = 0; spalte < BREITE; spalte++){
-                this.board[0][spalte].rahmenoben = true;
-                this.board[HOEHE-1][spalte].rahmenunten = true;
+                this.board[0][spalte].rahmenOben = true;
+                this.board[HOEHE-1][spalte].rahmenUnten = true;
             }
             for(let zeile = 0; zeile < HOEHE; zeile++){
-                this.board[zeile][0].rahmenlinks = true;
-                this.board[zeile][BREITE-1].rahmenrechts = true;
+                this.board[zeile][0].rahmenLinks = true;
+                this.board[zeile][BREITE-1].rahmenRechts = true;
             }
         },
-        layrinthsetzen(){
+        layrinthSetzen(){
             for(let zeile = 0; zeile < HOEHE; zeile++){                
                 for(let spalte = 0; spalte < BREITE; spalte++){
                     if(Math.random()<WAHRSCHEINLICHKEIT){
-                        this.board[zeile][spalte].rahmenoben = true;
+                        this.board[zeile][spalte].rahmenOben = true;
                     }                    
                     if(Math.random()<WAHRSCHEINLICHKEIT){
-                        this.board[zeile][spalte].rahmenrechts = true;
+                        this.board[zeile][spalte].rahmenRechts = true;
                     }                    
                     if(Math.random()<WAHRSCHEINLICHKEIT){
-                        this.board[zeile][spalte].rahmenunten = true;
+                        this.board[zeile][spalte].rahmenUnten = true;
                     }                    
                     if(Math.random()<WAHRSCHEINLICHKEIT){
-                        this.board[zeile][spalte].rahmenlinks = true;
+                        this.board[zeile][spalte].rahmenLinks = true;
                     }                    
+                }
+            }
+        },
+        nachbarnFinden(){
+            for(let zeile = 0; zeile < HOEHE; zeile++){                
+                for(let spalte = 0; spalte < BREITE; spalte++){
+                    const kasten = this.board[zeile][spalte];
+
+                    let oben = null;
+                    if(zeile>0){
+                        oben = this.board[zeile-1][spalte];
+                        if(!kasten.rahmenOben && ! oben.rahmenUnten){
+                            kasten.nachbarn.push(oben);
+                        }
+                    } 
+                    let rechts = null;
+                    if(spalte<BREITE-1){
+                        rechts = this.board[zeile][spalte+1];
+                        if(!kasten.rahmenRechts && ! rechts.rahmenLinks){
+                            kasten.nachbarn.push(rechts);
+                        }
+                    } 
+                     
+                    let unten = null;
+                    if(zeile<HOEHE-1){
+                        unten = this.board[zeile+1][spalte];
+                        if(!kasten.rahmenUnten && ! unten.rahmenOben){
+                            kasten.nachbarn.push(unten);
+                        }
+                    } 
+                    let links = null;
+                    if(spalte>0){
+                        links = this.board[zeile][spalte-1];
+                        if(!kasten.rahmenLinks && ! links.rahmenOben){
+                            kasten.nachbarn.push(unten);
+                        }
+                    }                     
                 }
             }
         }
@@ -58,8 +95,9 @@ const App = {
                 this.board[zeile].push(new Kasten());
             }
         }
-        this.rahmensetzen();
-        this.layrinthsetzen();
+        this.rahmenSetzen();
+        this.layrinthSetzen();
+        this.nachbarnFinden();
     }
 };
 Vue.createApp(App).mount('#app');
